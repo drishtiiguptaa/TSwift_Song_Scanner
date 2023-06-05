844af967448d821c75e878a6b61733f8cfbc4b7e
Drishti <drishtigupta@berkeley.edu>
2023-06-05 12:19:50 -0700

    case insensitive

--- song_scanner/tswift_song_scanner.py
+++ song_scanner/tswift_song_scanner.py
@@ -1,7 +1,9 @@
 from collections import Counter
+import re
 
 def can_form_song(title, available_letters):
-    title_counter = Counter(title.lower().replace(" ", ""))
+    title_letters = re.sub(r'[^a-zA-Z]', '', title.lower())
+    title_counter = Counter(title_letters)
     for letter, count in title_counter.items():
         if count > available_letters.get(letter, 0):
             return False
@@ -15,20 +17,23 @@ def get_possible_songs(song_titles, available_letters):
     return possible_songs
 
 # Example usage
-song_titles = [
-    "Hello",
-    "Shape of You",
-    "Bohemian Rhapsody",
-    "Yesterday",
-    "Don't Stop Believin'",
-    "Wonderwall"
+song_titles = [ "Tim McGraw", "Picture to Burn", "Teardrops on my Guitar", "A Place in this World", "The Outside", 
+               "Tied Together With a Smile", "Stay Beautiful", "Should've Said No", "Mary's Song", "Our Song", 
+               "I'm Only Me When I'm With You", "Invisible", "A Perfectly Good Heart"
 ]
 
 available_letters = {
     "a": 4,
     "l": 3,
     "o": 2,
-    "h": 1
+    "h": 1,
+    "m": 1,
+    "r": 1,
+    "y": 1,
+    "s": 2,
+    "n": 1,
+    "g": 1,
+    "u": 1
 }
 
 possible_songs = get_possible_songs(song_titles, available_letters)
